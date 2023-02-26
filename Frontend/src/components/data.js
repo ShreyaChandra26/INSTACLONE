dd1845e35aa32893f6704202a32f3be8adcba319
ShreyaChandra26 <gudiyamehta009@gmail.com>
2023-02-26 21:33:45 +0530

    File added

---
+++ Frontend/src/components/data.js
@@ -0,0 +1,27 @@
+
+
+
+const post=[
+    {name:"Siva",
+    location:"Bengaluru",
+    likes:64,
+    description: "Kick start your career",
+    PostImage: "./Mock-data/images/img1.jpg",
+    date: new Date(),
+    },
+    {name:"Neeraj",
+    location:"Pune",
+    likes:30,
+    description: "Sample Description",
+    PostImage: "./Mock-data/images/img2.jpg",
+    date: new Date(),
+    },
+    {name:"Rahul",
+    location:"Hyderabad",
+    likes:30,
+    description: "Sample Description for Post",
+    PostImage: "./Mock-data/images/img3.jpg",
+    date: new Date(),
+    }
+    ]
+    export default post
